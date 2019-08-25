15beaa5a72cf4fb4a31acff4ce673eae87504a58
Sabrina Rocha <delrock876@gmail.com>
2019-08-25 16:59:07 -0700

    prints pressed key to page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,3 +5,23 @@ const generateLetter = () => {
     randomLetter = char.charAt(Math.floor(Math.random() * char.length))
   return randomLetter
 }
+
+
+document.onkeyup = function (event){
+  let pressedKey = event.key
+document.getElementById('pressed').innerHTML += " " + pressedKey + " "
+}
+
+
+
+// const checkGuess
+
+// const guessesLeft = () => {
+//   let guesses = 8
+// if //guessedLetter
+
+//  for (let i=0; i <= guesses; i++ ){
+
+//  }
+
+//   }
