3c52df039fdbb1dbd5d0891f08facc418a510d02
Sabrina Rocha <delrock876@gmail.com>
2019-08-27 15:38:39 -0700

    reformatted & added comments for readability

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,17 +4,20 @@ let wins = 0
 let losses = 0
 let guessesLeft = 8
 
+document.getElementById('win').innerHTML = wins
+document.getElementById('loss').innerHTML = losses
+document.getElementById('guessesL').innerHTML = 8
+
 //generate random letter 
 const generateLetter = () => {
   randomLetter = char.charAt(Math.floor(Math.random() * char.length))
   return console.log(randomLetter)
 }
+
+//runs function when page loads
 window.onload = generateLetter()
 
-document.getElementById('win').innerHTML = wins
-document.getElementById('loss').innerHTML = losses
-document.getElementById('guessesL').innerHTML = 8
-
+//resets game
 const reset = () => {
   document.getElementById('guesses').innerHTML = ' '
   document.getElementById('guessesL').innerHTML = guessesLeft = 8
@@ -28,10 +31,9 @@ document.onkeyup = function printKey(event) {
   if (pressedKey === randomLetter) {
     alert("Congrats, you won!")
     document.getElementById('win').innerHTML = ++wins
-  reset()
+    reset()
 
   } else if (guessesLeft > 0) {
-   
     (document.getElementById('guesses').innerHTML += " " + pressedKey + " ")
     document.getElementById('guessesL').innerHTML = --guessesLeft
 
