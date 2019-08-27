76ae3accb5f2e60089e5630dc3441d33f8e28a9b
Sabrina Rocha <delrock876@gmail.com>
2019-08-27 14:24:52 -0700

    guessesleft decrement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,37 @@
 const char = 'abcdefghijklmnopqrstuvwxyz'
-let randomLetter  = ""
+let randomLetter = ""
 let currentGuess = []
 let wins = 1
 let losses = 1
+let guessesLeft = 9
 
 //generate random letter 
 const generateLetter = () => {
-    randomLetter = char.charAt(Math.floor(Math.random() * char.length))
+  randomLetter = char.charAt(Math.floor(Math.random() * char.length))
   return console.log(randomLetter)
 }
 window.onload = generateLetter()
 
 
-  document.onkeyup = function printKey(event) {
-    let pressedKey = event.key
+document.onkeyup = function printKey(event) {
+  let pressedKey = event.key
 
-  if(pressedKey === randomLetter){
+  if (pressedKey === randomLetter) {
     alert("Congrats, you won!")
     document.getElementById('win').innerHTML = wins++
     document.getElementById('guesses').innerHTML = ' '
+    guessesLeft = 9
     generateLetter()
-    
-  }else if (currentGuess.length < 9){
+
+  } else if (currentGuess.length < 9) {
     currentGuess.push(document.getElementById('guesses').innerHTML += " " + pressedKey + " ")
-  }else {
+    document.getElementById('guessesL').innerHTML = guessesLeft--
+  } else {
     alert('You lost! Try again :)')
     currentGuess = []
+    guessesLeft = 9
     document.getElementById('guesses').innerHTML = ' '
     document.getElementById('loss').innerHTML = losses++
     generateLetter()
   }
-  }
+}
